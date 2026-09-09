2f4f21f4ce19558ca5b9a33f2111ef4c77bbff0b
Davids Morales <dstmorales7@gmail.com>
2026-09-08 18:26:34 -0600

    Estructura inicial proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { Recipes } from './pages/recipes/recipes';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component:Home},
+    {path: 'contact', component:Contact},
+    {path: 'recipes', component: Recipes}
+    //string que se define para ponerlo en el browser como /home o /umes...
+];
+    
