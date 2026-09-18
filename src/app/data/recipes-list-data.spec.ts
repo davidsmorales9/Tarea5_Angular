df781486ff52e42a3f2ff6a53a6ff4bbeb8dfc5f
Davids Morales <dstmorales7@gmail.com>
2026-09-18 11:14:46 -0600

    Carpeta data con recipes-list-data

---
+++ src/app/data/recipes-list-data.spec.ts
@@ -0,0 +1,7 @@
+import { RecipesListData } from './recipes-list-data';
+
+describe('RecipesListData', () => {
+  it('should create an instance', () => {
+    expect(new RecipesListData()).toBeTruthy();
+  });
+});
